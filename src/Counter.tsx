f5b150b9342dcfe5b408bb222752ac82f8c8a435
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2023-01-09 03:34:13 +0900

    feat: counter component 제작

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react"
+
 const Counter = () => {
+  const [count, setCount] = useState(0)
   return (
-    <div>Counter</div>
+    <div>
+      count: {count}
+      <br/>
+      <button onClick={() => setCount(prev => prev+1)}>
+        increase
+      </button>
+      <button onClick={() => setCount(prev => prev-1)}>
+        reset
+      </button>
+    </div>
   )
 }
 export default Counter
