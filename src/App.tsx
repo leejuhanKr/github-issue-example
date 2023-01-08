cb17b4ae9feb144dc0efb1e6e862aefec2f0872c
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2023-01-09 03:51:19 +0900

    Feat: Counter Component 제작 (#4)
    * feat: init Counter Component
    
    * feat: counter component 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Counter from './Counter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Counter/>
     </div>
   )
 }
